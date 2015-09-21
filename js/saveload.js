149644de005cf9a08806daa0d365224f2bb77432
jschnurer <justinschnurer@gmail.com>
2015-09-21 14:14:34 -0400

    got saving and loading working

--- js/saveload.js
+++ js/saveload.js
@@ -7,32 +7,52 @@ saveLoader.prototype.doesSaveExist = function() {
 
 saveLoader.prototype.load = function() {
 	var savedGame = $.jStorage.get('saveGame');
-	var heroes = savedGame.party.heroes;
+	var heroes = [];
 	
-	for(var i = 0; i < heroes.length; i++) {
-		heroes[i] = new hero(heroes[i]);
+	for(var i = 0; i < 4; i++) {
+		// recreate hero objects from the saved heroes
+		heroes[i] = new hero(savedGame.party.heroes[i]);
+		
+		// recreate the spell objects from the saved spells
+		var spells = [];
+		for(var s = 0; s < heroes[i].spells.length; s++) {
+			if(heroes[i].spells[s])
+				spells[s] = new spell(heroes[i].spells[s]);
+		}
+		heroes[i].spells = spells;
 	}
 	
-	PARTY = new party(savedGame.party);
+	PARTY = new party({
+		experience: savedGame.party.experience,
+		heroes: heroes
+	});
 	
-	var items = savedGame.items;
-	for(var i = 0; i < items.length; i++) {
-		if(items[i] != undefined)
-			items[i] = new item(items[i]);
+	// recreate the item objects from the saved items
+	var items = [];
+	for(var i = 0; i < INVENTORY.maxLength; i++) {
+		if(savedGame.items[i] != undefined && savedGame.items[i] != null)
+			items[i] = new item(savedGame.items[i]);
 		else
 			items[i] = null;
 	}
 	INVENTORY = new inventory(items);
 	
-	gameVars = savedGame.gameVars;
+	GAME_VARS = savedGame.gameVars;
 	nav.teleportToMap(savedGame.mapId, savedGame.partyPosition.x, savedGame.partyPosition.y, savedGame.partyPosition.facing);
 }
 
 saveLoader.prototype.save = function(party, inventory, nav, gameVars) {
+	var itemsToSave = [];
+	for(var i = 0; i < INVENTORY.maxLength; i++) {
+		itemsToSave[i] = INVENTORY.getItemInPosition(i);
+		if(itemsToSave[i] == undefined)
+			itemsToSave[i] = null;
+	}
+	
 	var savedGame = {
 		party: party,
 		partyPosition: nav.getPartyPosition(),
-		items: inventory.items,
+		items: itemsToSave,
 		mapId: nav.getMapId(),
 		gameVars: gameVars
 	};
